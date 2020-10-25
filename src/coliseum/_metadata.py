3aad7154793de3482434671726120d416d21692c
hyperninja303 <hyperninja303@gmail.com>
2020-10-25 03:36:37 +0000

    Routine: added Agni as a Contributor

--- src/coliseum/_metadata.py
+++ src/coliseum/_metadata.py
@@ -1,6 +1,6 @@
 __author__ = "Sig Janoska-Bedi"
 __copyright__ = "Copyright (c) 2020 Signe Janoska-Bedi & Jamey Kane"
-__credits__ = ["Sig Janoska-Bedi", "Jamey Kane",]
+__credits__ = ["Sig Janoska-Bedi", "Jamey Kane", "Agni Bedi"]
 __license__ = "MIT"
 __maintainer__ = "Sig Janoska-Bedi"
 __email__ = "signe@siftingwinnowing.com"
